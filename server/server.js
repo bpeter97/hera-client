37e43cba4d7793fae8cfed9ba9ae8f0cd772247b
Brian Peter <brian.lee.peter@gmail.com>
2021-09-19 11:02:16 -0700

    remove unecessary files

--- server/server.js
+++ server/server.js
@@ -10,18 +10,10 @@ global.io = require("socket.io")(server, {
 		credentials: true,
 	},
 });
-const mongoose = require("mongoose");
 
 // middleware
 app.use(express.json());
 app.use(cors());
-
-// routes
-const tasks = require("./routes/api/tasks");
-const discord = require("./routes/api/discord");
-
-app.use("/api/tasks", tasks);
-app.use("/api/discord", discord);
 
 if (process.env.NODE_ENV === "production") {
 	// static folder
@@ -29,14 +21,6 @@ if (process.env.NODE_ENV === "production") {
 
 	app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
 }
-
-var db = process.env.DB_URI;
-
-// Connect to the DB.
-mongoose
-	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then(() => console.log("DB Connected"))
-	.catch((err) => console.error("DB Error", err));
 
 // declare port
 const port = process.env.PORT || 5000;
